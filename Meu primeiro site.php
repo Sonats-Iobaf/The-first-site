aabd9a0395da5f07efa14729a7aeef840e7af402
Sonats-Iobaf <73434802+Sonats-Iobaf@users.noreply.github.com>
2021-06-11 23:51:59 -0300

    Add files via upload

---
+++ Meu primeiro site.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<Style>
+	*{margin: 0px; padding: 0px; box-sizing: border-box; font-family: 'sans-serif;', sans-serif;}
+	.topo{
+		background: skyblue;
+		padding: 100px;
+		text-align: 20px;
+	}
+	h1{margin: 20px; padding: 20px; }
+	h2{margin: 20px; padding: 15px;}
+	p{margin: 20px; padding: 15px;}
+	img{margin: 20px; padding: 15px; image-rendering: 110;}
+	h3{margin: 20px; padding: 15px;}
+
+
+</Style>
+<html lang="pt-br"> 
+<head>
+	<meta charset="utf-8"> 
+	<meta name="viewport"
+	content="whidth=device-width, initial-scale=1.0">
+
+	<title>Parágrafo</title>
+	<style>
+		mark{
+			background-color: limegreen;
+		}
+	</style>
+</head>
+<body>
+	<div class="topo">
+		
+	</div>
+	<h1>Meu primeiro site...</h1>
+	
+	<p>Esse é meu primeiro documento em html, <br> php e css. Estou muito feliz. <br>
+
+	Esta, é uma linha de parárgrafo <br> você pode fazer quantas linhas de paragrafo <br> 
+	achar necessários ou precisar. <br>
+	Tambem pode usar a tag < br > para quebrar as linhas do paragrafo.</p>
+	<h2>Capítulo 1</h2>
+	<p>Preciso colocar margem inferior em todos os parágrafos de 15px <br>
+		margem inferior nos titulos de 20px quando for h2 e 15px quando for h3. <br>
+	Colocar underline nos links, transformar os links para que cada um fique em uma linha</p>
+	<h2>Colocando imagens!</h2>
+	<p>Abaixo você vai ver uma imagem que está na mesma pasta</p>
+	<img src="/sitedofabio/imagens/eru.jpg" alt="Dorminhoco">
+	</p>
+	<h2>1- Vamos adicionar alguns síbolos especiais: </h2>
+	<h2>
+		&reg;<br>
+		&copy;<br>
+		&trade;<br>
+		&euro;<br>
+		&#x1f596;
+		&#x1f913;
+	</h2>
+	<h3>Principais formatações</h3>
+	<p>Negrito / Destaque <br>
+	Nesta frase temos um <b>termo em negrito</b> usando a
+	tag B(não semântica). </p>
+	<p>Nesta frase, tenho um <strong>termo em destaque</strong> usando a tag STRONG (semântica).</p>
+	<h3>Italico / Ênfase</h3>
+	<p>Nesta frase, temo um <em>termo em ênfase</em> usando
+	a tag EM (semântica).</p>
+	<h3>Texto Marcado </h3>
+	<p>Podemos criar também <mark>um texto marcado </mark> usando a tag MARK.</p>
+	<p>E no outro paragrafo, temos <mark>outro texto 
+	marcado</mark> no final.</p>
+	<h3>Texto grande e pequeno</h3> 
+	<p>Estamos criando um <big>texto grande</big> e um texto pequeno nesse paragafo.</p>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
